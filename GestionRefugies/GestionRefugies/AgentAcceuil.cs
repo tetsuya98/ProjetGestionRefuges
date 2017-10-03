7150ce5bb7c026d25e9f27bfbcba1d605ed95166
Corentin Lesage <corentin.lesage@etud.u-picardie.fr>
2017-10-03 17:38:05 +0200

    modification de la classe user au niveau des attributs et utilsation de la classe user dans la classe agent accueil en tant que classe mere

--- GestionRefugies/GestionRefugies/AgentAcceuil.cs
+++ GestionRefugies/GestionRefugies/AgentAcceuil.cs
@@ -8,95 +8,29 @@ namespace GestionRefugies
 {
 
 
-    class AgentAcceuil
+    class AgentAcceuil : User
     {
         #region variables
         
-        /// <summary>
-        /// nom de l'Agent d'acceuil
-        /// </summary>
-        string nom;
-        /// <summary>
-        /// prénom de l'agent d'acceuil
-        /// </summary>
-        string prenom;
-        /// <summary>
-        /// identifiant de l'agent d'acceuil
-        /// </summary>
-        string id;
-        /// <summary>
-        /// mot de passe du réfugié
-        /// </summary>
-        string motdepasse;
         #endregion
-
+        
         #region constructeur
-        public AgentAcceuil(string nom, string prenom, string motdepasse)
+        
+        public AgentAcceuil(string nom,string prenom,string motdepasse,string id):base(nom,prenom,motdepasse)
         {
-            this.Nom = nom;
-            this.Prenom = prenom;
-            this.Motdepasse = motdepasse;
+             
+            Id = "AgA" + Nom + Prenom[0];
         }
 
         #endregion
 
         #region accesseurs
-        public string Nom
-        {
-            get
-            {
-                return nom;
-            }
-
-            set
-            {
-                nom = value;
-            }
-        }
-
-        public string Prenom
-        {
-            get
-            {
-                return prenom;
-            }
-
-            set
-            {
-                prenom = value;
-            }
-        }
-
-        public string Id
-        {
-            get
-            {
-                return id;
-            }
-
-            set
-            {
-                id = value;
-            }
-        }
-
-        public string Motdepasse
-        {
-            get
-            {
-                return motdepasse;
-            }
-
-            set
-            {
-                motdepasse = value;
-            }
-        }
+      
         #endregion
 
+        public
 
         #region méthode
-
 
 
 
