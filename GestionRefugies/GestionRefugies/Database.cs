0fdb0b289f9e4665ac379999bf9b045e44899581
Omer <omer.lakraa@etud.u-picardie.fr>
2017-10-09 18:35:50 +0200

    Création de la classe Database : connection à la base de donnée

---
+++ GestionRefugies/GestionRefugies/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GestionRefugies
+{
+    class Database
+    {
+        static MySqlConnection bd = null;
+
+        static MySqlConnection getBD()
+        {
+            if (bd == null)
+                bd = new MySqlConnection("database=test; server=localhost; user id=root; pwd=");
+            return bd;
+        }
+    }
+}
