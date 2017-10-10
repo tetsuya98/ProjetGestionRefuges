65b6871e94b733ba90aa14ac398924079bebe57b
Lariviere Odran <Lariviereo@etud.u-picardie.fr>
2017-10-10 10:28:44 +0200

    creation des Fonction SQL pour modif les refugier

---
+++ GestionRefugies/GestionRefugies/RefugierManage1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionRefugies
+{
+    partial class RefugierManage
+    {
+        public static void modifRefugier()
+        {
+            /*public static void ajouterRefugier(){
+            string sqlCommand = "INSERT INTO refugies (clef, prenom, nom, nationalite, mdp, camps_id, sexe, dateNais, adresse) VALUES ('clefy', 'prenom', 'nom', 'nationalite', 'mdp', 1, 'F', 'dateNais', 'Adresse')";
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+            MySqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            read.Close();
+        }
+
+---------------------------------------------------------------Nom----------------------------------------------------------------
+public static void modifierRefugierNom(Refugier a,string Nom)
+	
+            string sqlCommand = ("update refugies set nom ="+ Nom + " where nom = " + a.nom);
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+            MySqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            read.Close();
+        }
+---------------------------------------------------------------Prenom-------------------------------------------------------------
+
+public static void modifierRefugierPrenom(Refugier a,string Prenom)
+	
+            string sqlCommand = ("update refugies set prenom ="+ Prenom + " where prenom = " + a.prenom);
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+            MySqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            read.Close();
+        }
+---------------------------------------------------------------NATIONALITE--------------------------------------------------------
+
+public static void modifierRefugierNation(Refugier a,string Nationalite)
+	
+            string sqlCommand = ("update refugies set natinoalite ="+ Natinoalite + " where nationalite = " + a.natinoalite);
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+            MySqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            read.Close();
+        }
+---------------------------------------------------------------SEXE---------------------------------------------------------------
+
+public static void modifierRefugierSexe(Refugier a,string Sexe)
+	
+            string sqlCommand = ("update refugies set sexe =" + Sexe + " where sexe = " + a.sexe);
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+            MySqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            read.Close();
+        }
+
+
+---------------------------------------------------------------ADRESSE------------------------------------------------------------
+
+public static void modifierRefugierAdresse(Refugier a,string Adresse)
+	
+            string sqlCommand = ("update refugies set adresse ="+ Adresse + " where adresse = " + a.adresse);
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+            MySqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            read.Close();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Dans une class partiel de celle de roman .*/
+        }
+    }
+}
