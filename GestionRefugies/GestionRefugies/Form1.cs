a1a4a1b214c87f46f0d390ca8a4ba2e2c1e2ab06
Yann Flipo <yann.flipo@etud.u-picardie.fr>
2017-10-10 09:34:27 +0200

    Avancement Premiere form

--- GestionRefugies/GestionRefugies/Form1.cs
+++ GestionRefugies/GestionRefugies/Form1.cs
@@ -10,11 +10,59 @@ using System.Windows.Forms;
 
 namespace GestionRefugies
 {
-    public partial class Form1 : Form
+    public partial class Connexion : Form
     {
-        public Form1()
+        public Connexion()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Txt_Adress.Text = /*appel de BD*/ "Adresse du Camps";
+
+        }
+
+        private void Btn_Valider_Click(object sender, EventArgs e)
+        {
+            if (Txt_ID.Text != null && Txt_ID.Text != "Identifiant" && Txt_MDP.Text != null && Txt_MDP.Text != "Mot de Passe")
+            {
+
+                //appel BD
+                /*if (BD ok){ this.close; prochainePage.show}
+                  else{}*/
+            }
+        }
+
+       
+        private void Txt_ID_TextChanged(object sender, EventArgs e)
+        {
+            this.Txt_ID.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void Txt_MDP_TextChanged(object sender, EventArgs e)
+        {
+            //this.Txt_MDP.PasswordChar.ToString("•");
+            this.Txt_MDP.ForeColor = System.Drawing.Color.Black;
+
+        }
+
+        private void Txt_ID_Click(object sender, EventArgs e)
+        {
+            if (this.Txt_ID.Text == "Identifiant" )
+            {
+                this.Txt_ID.Text = "";
+            }
+            
+        }
+
+        private void Txt_MDP_Click(object sender, EventArgs e)
+        {
+            if (this.Txt_MDP.Text == "Mot de Passe")
+            {
+                this.Txt_MDP.Text = "";
+            }
+        }
+
     }
 }
