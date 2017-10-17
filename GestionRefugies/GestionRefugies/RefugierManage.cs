25f35bf18a11602883b00a2c44a8a61d59ab57c1
RomanC <roman.cottinet@etud.u-picardie.fr>
2017-10-17 09:00:17 +0200

    ajout package my_sql

--- GestionRefugies/GestionRefugies/RefugierManage.cs
+++ GestionRefugies/GestionRefugies/RefugierManage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient; //dans la console des package Update-Package -reinstall
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,15 @@ using System.Threading.Tasks;
 
 namespace GestionRefugies
 {
-    class RefugierManage
+    partial class RefugierManage
     {
-
+            public static void ajouterRefugier()
+            {
+                string sqlCommand = "INSERT INTO refugies (clef, prenom, nom, nationalite, mdp, camps_id, sexe, dateNais, adresse) VALUES ('clefy', 'prenom', 'nom', 'nationalite', 'mdp', 1, 'F', 'dateNais', 'Adresse')";
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+                MySqlDataReader read = cmd.ExecuteReader();
+                read.Read();
+                read.Close();
+            }      
     }
 }
