a52aabc98c38d82d5b89ba65554a1d2bcaaf3379
JosselinAbel <josselin.abel@etud.u-picardie.fr>
2017-10-17 10:13:17 +0200

    Lien bdd classe maga

---
+++ GestionRefugies/GestionRefugies/MagasinierManage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionRefugies
+{
+    class MagasinierManage
+    {
+        public static void ajouterMagasinier(string login, string nom, string adresse)
+        {
+            string sqlCommand = $"INSERT INTO magasinier (clef, nom, adresse) VALUES ('{login}', '{nom}', '{adresse})";
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+            MySqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            read.Close();
+        }
+
+        public static void supprimerMagasinier(string login)
+        {
+            string sqlCommand = $"DELETE FROM magasinier WHERE clef = '{login}'";
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+            MySqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            read.Close();
+        }
+
+        public static void updateMagasinier(string login, string nom, string adresse)
+        {
+            string sqlCommand = $"UPDATE magasinier nom = '{nom}', adresse = '{adresse}' WHERE clef = '{login}'";
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+            MySqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            read.Close();
+        }
+    }
+}
