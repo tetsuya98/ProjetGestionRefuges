02099e9f55072d38a4960a22a0c1885d2ceef54b
Omer <omer.lakraa@etud.u-picardie.fr>
2017-11-22 21:24:36 +0100

    Ajout du role Agent

---
+++ GestionRefugies/GestionRefugies/role/Magasinier.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionRefugies.role
+{
+    class Magasinier : Role
+    {
+        #region champs
+        #endregion
+
+        #region propriete
+        #endregion
+
+        #region methode
+        /// <summary>
+        /// Ajoute les informations du magasinier dans la base de donnée
+        /// </summary>
+        /// <returns>Retourne false si la requete echoue, sinon retourne vrai</returns>
+        public override bool ajouter()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Modifie les informations du magasinier dans la base de donnée
+        /// </summary>
+        /// <returns>Retourne false si la requete echoue, sinon retourne vrai</returns>
+        public override bool modifier()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Supprime les informations le magasinier dans la base de donnée
+        /// </summary>
+        /// <returns>Retourne false si la requete echoue, sinon retourne vrai</returns>
+        public override bool supprimer()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Récupère les informations sur le magasinier dans la base de donnée
+        /// </summary>
+        /// <returns>Retourne un objet magasinier</returns>
+        public override List<Role> affiche()
+        {
+            return null;
+        }
+        #endregion
+    }
+}
