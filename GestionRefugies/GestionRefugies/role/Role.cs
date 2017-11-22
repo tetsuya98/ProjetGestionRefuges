6db40280e49dedee3ae17e30b5c90fc27e27ca27
Omer <omer.lakraa@etud.u-picardie.fr>
2017-11-22 20:39:20 +0100

    Ajout du role Magasinier

--- GestionRefugies/GestionRefugies/role/Role.cs
+++ GestionRefugies/GestionRefugies/role/Role.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace GestionRefugies
 {
-    class Role
+    abstract class Role
     {
+        #region champs
+        private String nom;
+        #endregion
+
+        #region propriete
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+        }
+        #endregion
+
+        #region methode
+
+        /// <summary>
+        /// Ajoute les informations du role dans la base de donnée
+        /// </summary>
+        /// <returns>Retourne false si la requete echoue, sinon retourne vrai</returns>
+        public abstract bool ajouter();
+
+        /// <summary>
+        /// Modifie les informations du role dans la base de donnée
+        /// </summary>
+        /// <returns>Retourne false si la requete echoue, sinon retourne vrai</returns>
+        public abstract bool modifier();
+
+        /// <summary>
+        /// Supprime les informations du role dans la base de donnée
+        /// </summary>
+        /// <returns>Retourne false si la requete echoue, sinon retourne vrai</returns>
+        public abstract bool supprimer();
+
+        /// <summary>
+        /// Récupère les informations sur le role dans la base de donnée
+        /// </summary>
+        /// <returns>Retourne un objet role</returns>
+        public abstract List<Role> affiche();
+
+        #endregion
+
     }
 }
