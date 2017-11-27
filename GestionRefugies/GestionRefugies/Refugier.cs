84f36a82344df2c957e04730dafa3ff80903d55e
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2017-11-27 19:02:34 +0100

    Modification de la fonction add dans la classe refugier

--- GestionRefugies/GestionRefugies/Refugier.cs
+++ GestionRefugies/GestionRefugies/Refugier.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace GestionRefugies
 {
@@ -32,8 +33,8 @@ namespace GestionRefugies
         /// <summary>
         /// Date de naissance du refugier
         /// </summary>
-        private DateTime date_nais;
-        
+        private DateTime dateNais;
+
         /// <summary>
         /// Adresse du refugier
         /// </summary>
@@ -75,11 +76,11 @@ namespace GestionRefugies
                 return sexe;
             }
         }
-        public DateTime Date_nais
+        public DateTime DateNais
         {
             get
             {
-                return date_nais;
+                return dateNais;
             }
         }
         public int Adresse
@@ -99,16 +100,47 @@ namespace GestionRefugies
         #endregion
 
         #region methode
-        public Refugier(string nom, string prenom,string motdepasse, string sexe, DateTime date_nais, string nationalite, int adresse, int id = 0)
+        public Refugier(string nom, string prenom, string motdepasse, string sexe, DateTime dateNais, string nationalite, int adresse, int id = 0)
         {
             this.nom = nom;
             this.prenom = prenom;
             this.sexe = sexe;
-            this.date_nais = date_nais;
+            this.dateNais = dateNais;
             this.nationalite = nationalite;
             this.adresse = adresse;
             this.id = id;
         }
-        #endregion
+
+        public static bool add(Refugier refugier)
+        {
+
+            ///finir la requete sql
+            string sqlCommand = "INSERT INTO refugiers (nom, prenom, nationalite, sexe, adresse, dateNais) VALUES (?,?,?,?,?,?)";
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, Database.getBD());
+
+            cmd.CommandText = sqlCommand;
+
+            //Envoi des paramètres
+            cmd.Parameters.AddWithValue("@nom", refugier.Nom);
+            cmd.Parameters.AddWithValue("@prenom", refugier.Prenom);
+            cmd.Parameters.AddWithValue("@nationalite", refugier.Nationalite);
+            cmd.Parameters.AddWithValue("@sexe", refugier.Sexe);
+            cmd.Parameters.AddWithValue("@adresse", refugier.Adresse);
+            cmd.Parameters.AddWithValue("@dateNais", refugier.DateNais.Date.ToString("yyyy-MM-dd"));
+            
+
+            try
+            {
+                //Execution de la commande SQL qui peut provoquer des exceptions
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //traitement de l'exception...
+                return false;
+            }
+            #endregion
+        }
     }
 }
