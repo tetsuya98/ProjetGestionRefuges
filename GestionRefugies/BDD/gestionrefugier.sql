62a3754506989de7a2234a0134890599d21ee915
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2017-12-05 08:08:01 +0100

    Ajout de la table items dans la base de donnée

--- GestionRefugies/BDD/gestionrefugier.sql
+++ GestionRefugies/BDD/gestionrefugier.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 02, 2017 at 11:41 AM
+-- Generation Time: Dec 05, 2017 at 07:06 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -27,6 +27,22 @@ use gestionrefugier;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE IF NOT EXISTS `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `description` int(255) NOT NULL,
+  `datePeremtion` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `nom` (`nom`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `refugiers`
 --
 
@@ -42,19 +58,20 @@ CREATE TABLE IF NOT EXISTS `refugiers` (
   PRIMARY KEY (`id`),
   KEY `nom` (`nom`),
   KEY `prenom` (`prenom`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `refugiers`
 --
 
 INSERT INTO `refugiers` (`id`, `nom`, `prenom`, `nationalite`, `sexe`, `adresse`, `dateNais`) VALUES
-(2, 'Lakraa', 'Omer', 'français', 'M', 3, '2017-11-27'),
-(3, 'e', 'e', 'français', 'M', 3, '2017-11-27'),
+(3, 'Lakraa', 'Oumer', 'français', 'M', 3, '2017-12-02'),
 (4, 'Flipo', 'Yann', 'français', 'F', 2, '2017-11-08'),
 (5, 'Lakraa', 'Lakraa', 'français', 'M', 3, '2017-11-27'),
 (6, 'Lakraa', 'Lakraa', 'français', 'M', 3, '2017-11-27'),
-(7, 'Lakraa', 'Omer', 'français', 'M', 3, '2017-11-27');
+(7, 'Lakraa', 'Omer', 'français', 'M', 3, '2017-11-27'),
+(8, 'Lakraa', 'Omer', 'français', 'M', 3, '2017-12-02'),
+(9, 'Lakraa', 'Omer', 'français', 'M', 3, '2017-12-02');
 
 -- --------------------------------------------------------
 
@@ -82,8 +99,23 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`nom`, `prenom`, `agent`, `magasinier`, `administrateur`, `mdp`, `login`) VALUES
-('Lakraa', 'Omer', 1, 0, 0, 'mdp', 'lakraao'),
-('Flipo', 'Yann', 0, 1, 1, 'mdp', 'flipoy');
+('Bonjour', 'Elvire', 1, 0, 1, '1dxYKgHDM4M1kI9bSy7JaZwL6fwCFPQ/LAM5ZHXOJ1ta7lVyM2//GHBkKKiyJNeGmR0nbPVJls3MABZgYchRrw==', 'lakraao'),
+('Flipo', 'Yann', 0, 1, 1, 'mdp', 'flipoy'),
+('Lakraa', 'Omer', 0, 0, 1, 'QVvfWX+DEokiN+LjzwOXIouluFRtjXZ4FL+rktslEEM4PnteKj3FAiIaNt3aZFzBsTAgt6fmck5lcTGtcqyXJA==', 'lakraao476'),
+('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao1011566107'),
+('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao1529273179'),
+('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao1627656101'),
+('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao873'),
+('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao107'),
+('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao658'),
+('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao456'),
+('Lakraa', 'Omer', 0, 0, 1, 'fyake+G/qdbB3VzFh8xrNjKTd8jdFmbHclyQCsVaF+eUKpnTmzsci6oFhwJmQSQnQOOCA1Qq4LE/Y9hiZYK2rw==', 'lakraao2'),
+('Bonjour', 'Salut', 0, 0, 1, 'Ho53yEKuCFRHeuJzUz2tAr08/U1auEX6lfeT/6op2BHeo6odgOjdipa1d9aLjoM2lYyDzf17veWbiQ6mNxpRsA==', 'bonjours974'),
+('user', 'user', 0, 0, 0, '5vfPAmPGcMqIggDCuGZu/T7bV2lFXRqHzfszCTQ+9ukcADqcu6OG6a2xMU9xx083PHJEz2at8yoxDvvVQ4VL1Q==', 'useru637'),
+('user', 'user', 0, 0, 0, 'zYh6Zj7I3froYDu0FCEdU84/ovVtn3vpo+URohLD+CaGkK37rnVZF4Xf4igN+hFAJT+lDpQw6zUUGUG1pldbSg==', 'useru328'),
+('user', 'user', 0, 0, 0, 'ZEpvJz10dFiIoYnslzQgDA/KDmtKPPIXI6d6gai63guwLJ40cBJIIJBMsv2tT/o9gNabiXtTLxX39rxWUIyInA==', 'useru266'),
+('user', 'user', 0, 0, 0, 'yPpFgpIOb103KNYhfbEMBm9xgJoNTxjZr934J2FAkfl6+64EV3ELdrDbuiHUIBx4cbvEPf2rfmXXCUnLjr5RkA==', 'useru803'),
+('user', 'user', 0, 0, 0, '1GuYutcFvVbqkiFRQhjaynkuk05tV4aiK8azjzPOa08mWJZy5XTBM4l6cJzIgvA3IYO4yOIaVKkweGTxBliS5g==', 'useru103');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
