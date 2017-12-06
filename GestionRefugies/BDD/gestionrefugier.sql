562b8b751b23b685eb50c5a984a701eccde3cce5
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2017-12-06 19:05:04 +0100

    Ajout de la table camps et des relations refugiers camps et users camps

--- GestionRefugies/BDD/gestionrefugier.sql
+++ GestionRefugies/BDD/gestionrefugier.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 05, 2017 at 06:02 PM
+-- Generation Time: Dec 06, 2017 at 06:03 PM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -24,6 +24,22 @@ SET time_zone = "+00:00";
 DROP DATABASE IF EXISTS gestionrefugier;
 CREATE DATABASE gestionrefugier CHARACTER SET 'utf8';
 use gestionrefugier;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `camps`
+--
+
+DROP TABLE IF EXISTS `camps`;
+CREATE TABLE IF NOT EXISTS `camps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `capacite` int(11) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_2` (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- --------------------------------------------------------
 
 --
@@ -64,19 +80,12 @@ CREATE TABLE IF NOT EXISTS `refugiers` (
   `allergie` varchar(255) NOT NULL,
   `handicap` varchar(255) NOT NULL,
   `autre` varchar(255) NOT NULL,
+  `id_camps` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `nom` (`nom`),
-  KEY `prenom` (`prenom`)
+  KEY `prenom` (`prenom`),
+  KEY `fk_refugiers_camps` (`id_camps`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `refugiers`
---
-
-INSERT INTO `refugiers` (`id`, `nom`, `prenom`, `nationalite`, `sexe`, `adresse`, `dateNais`, `taille`, `couleurPeau`, `couleurCheveux`, `typeCheveux`, `couleurYeux`, `blessure`, `allergie`, `handicap`, `autre`) VALUES
-(1, 'Lakraa', 'Omer', 'français', 'M', 3, '2017-12-05', 3, 'blue', 'rouge', 'bouclé', 'Orange', 'Non', 'math', 'con', 'autre....'),
-(2, 'Lakraa', 'Omer', 'français', 'M', 3, '2017-12-05', 3, 'blue', 'rouge', 'bouclé', 'Orange', 'Non', 'math', 'con', 'autre....'),
-(3, 'Lakraa', 'Omer', 'français', 'M', 3, '2017-12-05', 3, 'blue', 'rouge', 'bouclé', 'Orange', 'Non', 'math', 'con', 'autre....');
 
 -- --------------------------------------------------------
 
@@ -93,39 +102,29 @@ CREATE TABLE IF NOT EXISTS `users` (
   `administrateur` tinyint(1) NOT NULL,
   `mdp` varchar(255) NOT NULL,
   `login` varchar(255) NOT NULL,
+  `id_camps` int(11) NOT NULL,
   PRIMARY KEY (`login`),
   UNIQUE KEY `login` (`login`),
   KEY `login_2` (`login`),
-  KEY `login_3` (`login`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  KEY `login_3` (`login`),
+  KEY `fk_users_camps` (`id_camps`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `users`
+-- Constraints for dumped tables
 --
 
-INSERT INTO `users` (`nom`, `prenom`, `agent`, `magasinier`, `administrateur`, `mdp`, `login`) VALUES
-('Bonjour', 'Elvire', 1, 0, 1, '1dxYKgHDM4M1kI9bSy7JaZwL6fwCFPQ/LAM5ZHXOJ1ta7lVyM2//GHBkKKiyJNeGmR0nbPVJls3MABZgYchRrw==', 'lakraao'),
-('Flipo', 'Yann', 0, 1, 1, 'mdp', 'flipoy'),
-('Lakraa', 'Omer', 0, 0, 1, 'QVvfWX+DEokiN+LjzwOXIouluFRtjXZ4FL+rktslEEM4PnteKj3FAiIaNt3aZFzBsTAgt6fmck5lcTGtcqyXJA==', 'lakraao476'),
-('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao1011566107'),
-('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao1529273179'),
-('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao1627656101'),
-('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao873'),
-('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao107'),
-('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao658'),
-('Lakraa', 'Omer', 0, 0, 1, '59nvWRCa1zW4ZDjtt31l1jCTtUPfoU5gLuUkfqGJ+EQUZqGGcYIwgFmpX+vKFBFmcHGDy9jOfWjfJ1jbaisliQ==', 'lakraao456'),
-('Lakraa', 'Omer', 0, 0, 1, 'fyake+G/qdbB3VzFh8xrNjKTd8jdFmbHclyQCsVaF+eUKpnTmzsci6oFhwJmQSQnQOOCA1Qq4LE/Y9hiZYK2rw==', 'lakraao2'),
-('Bonjour', 'Salut', 0, 0, 1, 'Ho53yEKuCFRHeuJzUz2tAr08/U1auEX6lfeT/6op2BHeo6odgOjdipa1d9aLjoM2lYyDzf17veWbiQ6mNxpRsA==', 'bonjours974'),
-('user', 'user', 0, 0, 0, '5vfPAmPGcMqIggDCuGZu/T7bV2lFXRqHzfszCTQ+9ukcADqcu6OG6a2xMU9xx083PHJEz2at8yoxDvvVQ4VL1Q==', 'useru637'),
-('user', 'user', 0, 0, 0, 'zYh6Zj7I3froYDu0FCEdU84/ovVtn3vpo+URohLD+CaGkK37rnVZF4Xf4igN+hFAJT+lDpQw6zUUGUG1pldbSg==', 'useru328'),
-('user', 'user', 0, 0, 0, 'ZEpvJz10dFiIoYnslzQgDA/KDmtKPPIXI6d6gai63guwLJ40cBJIIJBMsv2tT/o9gNabiXtTLxX39rxWUIyInA==', 'useru266'),
-('user', 'user', 0, 0, 0, 'yPpFgpIOb103KNYhfbEMBm9xgJoNTxjZr934J2FAkfl6+64EV3ELdrDbuiHUIBx4cbvEPf2rfmXXCUnLjr5RkA==', 'useru803'),
-('user', 'user', 0, 0, 0, '1GuYutcFvVbqkiFRQhjaynkuk05tV4aiK8azjzPOa08mWJZy5XTBM4l6cJzIgvA3IYO4yOIaVKkweGTxBliS5g==', 'useru103'),
-('user', 'user', 0, 0, 0, 'sE/j4jDD/79Iy8CkxrInPt+1dIRR1Yz404kopwoWsJdQMNqPOBHhyOfGQ2fTwNAB8j+kStheehAjV2wIGI+hgg==', 'useru361'),
-('user', 'user', 0, 0, 0, 'Fuxvftxu+I+MIauwvOxwzBogODEqAxXHGOemUBKsstjW5IkOqssqv6NXnvIKcbfKu7vlI9n5WFgTLiUFIs5I+w==', 'useru404'),
-('user', 'user', 0, 0, 0, 'vumSytlEnMelfWVK2v2TtPtoMdoDB6P/yhSBgL/0WZ6y2qSRNH/HkGs7FiE//KbCrh6a1fkOE5yJ1bXQSUxOow==', 'useru848'),
-('user', 'user', 0, 0, 0, 'O8NUT2qizyyoJnIKBiGlo4XVGJWrMtMVJybEoVXjgxm/PgYS/JioMdRV84ssE+xhD5jEeK4ziMJSeLZ8udEgnQ==', 'useru441'),
-('user', 'user', 0, 0, 0, 'U6RCnUw8O2+/IBZB1KCWP/+1JR0HjBqkNRCVM6s+VIatWAx6UkD2RGuYTV34XmSDzMw6mmrGlrQ9ve443HX5vw==', 'useru739');
+--
+-- Constraints for table `refugiers`
+--
+ALTER TABLE `refugiers`
+  ADD CONSTRAINT `fk_refugiers_camps` FOREIGN KEY (`id_camps`) REFERENCES `camps` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_users_camps` FOREIGN KEY (`id_camps`) REFERENCES `camps` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
