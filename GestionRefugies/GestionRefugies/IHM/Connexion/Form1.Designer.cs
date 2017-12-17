d3d93a6b4b5fc2c5675baea882cdfaf255738f23
Roman Cottinet <roman.cottinet@etud.u-picardie.fr>
2017-12-17 14:52:05 +0100

    debut map

--- GestionRefugies/GestionRefugies/IHM/Connexion/Form1.Designer.cs
+++ GestionRefugies/GestionRefugies/IHM/Connexion/Form1.Designer.cs
@@ -35,6 +35,10 @@
             this.Lbl_Alert = new System.Windows.Forms.Label();
             this.Txt_MDP = new System.Windows.Forms.MaskedTextBox();
             this.Txt_ID = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,7 +47,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.Control;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(53, 96);
+            this.label1.Location = new System.Drawing.Point(40, 49);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -54,8 +58,8 @@
             // 
             // Btn_Valider
             // 
-            this.Btn_Valider.Location = new System.Drawing.Point(137, 260);
-            this.Btn_Valider.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Btn_Valider.Location = new System.Drawing.Point(300, 179);
+            this.Btn_Valider.Margin = new System.Windows.Forms.Padding(4);
             this.Btn_Valider.Name = "Btn_Valider";
             this.Btn_Valider.Size = new System.Drawing.Size(100, 28);
             this.Btn_Valider.TabIndex = 5;
@@ -67,7 +71,7 @@
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pictureBox1.Location = new System.Drawing.Point(419, 49);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(443, 255);
             this.pictureBox1.TabIndex = 6;
@@ -77,7 +81,7 @@
             // 
             this.Txt_Adress.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Txt_Adress.Location = new System.Drawing.Point(419, 342);
-            this.Txt_Adress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_Adress.Margin = new System.Windows.Forms.Padding(4);
             this.Txt_Adress.Name = "Txt_Adress";
             this.Txt_Adress.ReadOnly = true;
             this.Txt_Adress.Size = new System.Drawing.Size(444, 15);
@@ -88,7 +92,7 @@
             // 
             this.Lbl_Alert.AutoSize = true;
             this.Lbl_Alert.ForeColor = System.Drawing.Color.DarkRed;
-            this.Lbl_Alert.Location = new System.Drawing.Point(40, 329);
+            this.Lbl_Alert.Location = new System.Drawing.Point(40, 190);
             this.Lbl_Alert.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Alert.Name = "Lbl_Alert";
             this.Lbl_Alert.Size = new System.Drawing.Size(244, 17);
@@ -100,8 +104,8 @@
             // 
             this.Txt_MDP.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.Txt_MDP.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.Txt_MDP.Location = new System.Drawing.Point(44, 207);
-            this.Txt_MDP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_MDP.Location = new System.Drawing.Point(44, 137);
+            this.Txt_MDP.Margin = new System.Windows.Forms.Padding(4);
             this.Txt_MDP.Name = "Txt_MDP";
             this.Txt_MDP.Size = new System.Drawing.Size(293, 22);
             this.Txt_MDP.TabIndex = 9;
@@ -114,8 +118,8 @@
             // 
             this.Txt_ID.Cursor = System.Windows.Forms.Cursors.Default;
             this.Txt_ID.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.Txt_ID.Location = new System.Drawing.Point(44, 150);
-            this.Txt_ID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_ID.Location = new System.Drawing.Point(43, 92);
+            this.Txt_ID.Margin = new System.Windows.Forms.Padding(4);
             this.Txt_ID.Name = "Txt_ID";
             this.Txt_ID.Size = new System.Drawing.Size(293, 22);
             this.Txt_ID.TabIndex = 10;
@@ -123,12 +127,56 @@
             this.Txt_ID.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txt_ID_KeyDown);
             this.Txt_ID.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Txt_ID_KeyUp);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 297);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 22);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "Ville";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(44, 342);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(293, 22);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.Text = "Pays";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 255);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(47, 24);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Map";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(300, 380);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Rechercher";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form_Connexion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(912, 444);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Txt_ID);
             this.Controls.Add(this.Txt_MDP);
             this.Controls.Add(this.Lbl_Alert);
@@ -136,7 +184,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Btn_Valider);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form_Connexion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Connexion";
@@ -156,6 +204,10 @@
         private System.Windows.Forms.Label Lbl_Alert;
         private System.Windows.Forms.MaskedTextBox Txt_MDP;
         private System.Windows.Forms.MaskedTextBox Txt_ID;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
