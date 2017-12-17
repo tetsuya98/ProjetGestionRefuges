90d43a04a8a5e9e73149fd4e7e43d8e963f19746
Roman Cottinet <roman.cottinet@etud.u-picardie.fr>
2017-12-17 17:20:58 +0100

    maptest

--- GestionRefugies/GestionRefugies/IHM/Connexion/Form1.cs
+++ GestionRefugies/GestionRefugies/IHM/Connexion/Form1.cs
@@ -27,7 +27,7 @@ namespace GestionRefugies
         /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
-            StringBuilder adresse = new StringBuilder("https://www.google.com/maps/");
+            StringBuilder adresse = new StringBuilder("https://www.google.com/maps?q=");
             webBrowser1.Navigate(adresse.ToString());
             this.Txt_Adress.Text = /*appel de BD*/ "Adresse du Camps";
         } 
